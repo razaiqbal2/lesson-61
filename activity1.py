19145d5fe1571483edad3debc4400bb81ec596e5
razaiqbal2 <duaf0226@gmail.com>
2025-03-17 21:02:46 +0500

    activity1

---
+++ activity1.py
@@ -0,0 +1,9 @@
+num1=4
+num2=5
+
+print(num1,'and ',num2,'is',(num1&num2))
+print(num1,'or ',num2,'is',(num1|num2))
+print(num1,'xor ',num2,'is',(num1^num2))
+print(num1,'not ',num1,'is',~num1)
+print(num1,'rigth shift by 1 is ',(num1>>1))
+print(num1,'left shift by 1 is',(num2<<1))
